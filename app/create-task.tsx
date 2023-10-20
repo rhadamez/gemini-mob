7a02e7ece8cbf8485041fef39a85ba0ffea2922f
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-10-20 08:22:56 -0300

    feat: add react-hook-form

--- app/create-task.tsx
+++ app/create-task.tsx
@@ -2,14 +2,32 @@ import { VStack } from 'native-base'
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useTasks } from '../context/TasksContext';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function CreateTask() {
+  const { addTask } = useTasks()
+
+  const { control, handleSubmit, reset, clearErrors, setFocus, formState: { errors } } = useForm({
+		mode: 'all'
+	})
+
+  function handleAddTask(data: any) {
+    console.log(data)
+  }
+
   return (
     <VStack bg='#011323' flex={1} justifyContent={'center'}>
       <Header />
       <VStack m={5}>
-        <Input placeholder='description' />
-        <Button text='Add task'/>
+        <Controller
+          control={control}
+          name='description'
+          render={({ field: { onChange, value }}) => (
+            <Input placeholder='description' onChangeText={onChange} value={value} />
+          )}
+        />
+        <Button text='Add task' onPress={handleSubmit(handleAddTask)} />
       </VStack>
     </VStack>
   );
